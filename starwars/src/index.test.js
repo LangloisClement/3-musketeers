fd2458c36a0b9784d7d17c414a9e0fbb335a9b09
LangloisClement <langlois.clement@gamil.com>
2019-03-05 13:13:42 +0100

    starwars test: first 5 are working

--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -7,25 +7,35 @@ describe('starwars-names', () => {
       expect(starWars.all).toHaveLength(starWarsNames.length)
     });
     test('should be an array of strings', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      for(var i = 0; i < starWarsNames.length; i++){
+        expect(starWarsNames[i]).toMatch(/.+/);
+      }
     });
 
-    test('should contain `Luke Skywalker`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+    test('should contain Luke Skywalker', () => {
+      expect(starWars.all).toContain('Luke Skywalker');
     });
 
-    test('should not contain `Ben Quadinaros`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+    test('should not contain Ben Quadinaros', () => {
+      expect(starWars.all).not.toContain('Ben Quadinaros');
     });
   });
 
   describe('random', () => {
     test('should return a random item from the starWars.all', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      let random = Math.floor(Math.random() * Math.floor(starWars.all.length));
+      expect(starWars.all[random]);
     });
 
-    test('should return an array of random items if passed a number', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+    test('should return an array of random items if passed a number', (entry) => {
+      let list = [];
+      if(parseInt(entry, 10)){
+        for(let i = 0; i < starWars.all.length; i++) {
+          let random = Math.floor(Math.random() * Math.floor(starWars.all.length));
+          list.push(starWars.all[random]);
+          expect(list[i]).toMatch(starWars.all[random]);
+        }
+      }
     });
   });
 });
